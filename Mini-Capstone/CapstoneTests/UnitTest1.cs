aa59e34ced10eb6e2c90313e6f20f029a62d31cc
Alec Iberg <ibergalec@gmail.com>
2019-02-08 11:04:59 -0500

    preparing to pull partial tests

--- Mini-Capstone/CapstoneTests/UnitTest1.cs
+++ Mini-Capstone/CapstoneTests/UnitTest1.cs
@@ -1,13 +1,26 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Capstone.Classes;
+using System.Collections.Generic;
 
 namespace CapstoneTests
 {
     [TestClass]
     public class UnitTest1
     {
+
         [TestMethod]
-        public void TestMethod1()
+        public void TypeBoughtTest()
         {
+            VendingMachine vm = new VendingMachine();
+            vm.ReadFile();
+            //List<VendingMachineItem> items = new List<VendingMachineItem>();
+            //VendingMachineItem vendingMachineItem = new VendingMachineItem();
+            //vendingMachineItem.SlotLocation = "A1";
+            //vendingMachineItem.SnackName = "Potato Crisps";
+            //vendingMachineItem.Price = 1.65M;
+            //items.Add(vendingMachineItem);
+            string test = vm.TypeBought("A1");
+            Assert.AreEqual("A", test);
         }
     }
 }
